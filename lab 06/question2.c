a3b3befedfe590e014f033789facd0ec5dff78ba
Zaidi-Alisha <142868085+Zaidi-Alisha@users.noreply.github.com>
2023-10-03 10:34:30 +0500

    Update question2.c

--- lab 06/question2.c
+++ lab 06/question2.c
@@ -10,12 +10,12 @@ int main() {
             int previous = 0; //first number
             int current = 1; //second number
             int fib = 0; // previous + current
-            sum = 0;
+            int sum = 0;
            printf("Fibonacci sequence till 10000");
             for (int count = 0; fib <= 10000; count++) {
             printf("%d ", fib);
             fib = previous + current; 
-            previous = current
+            previous = current;
             current = fib;
         if ( fib % 3 == 0 || fib % 5 == 0 || fib % 7 == 0 ) {
             sum += fib;
