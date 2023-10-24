9726236b2ae11769587e5bba87d977e2a93d0a41
Zaidi-Alisha <142868085+Zaidi-Alisha@users.noreply.github.com>
2023-10-24 09:24:14 +0500

    Add files via upload

---
+++ lab 8/questions.c
@@ -0,0 +1,33 @@
+/**
+ *Programmer: Alisha Zaidi
+ *Date: 24 October, 2023
+ *Desc: Make a function and then swap two numbers
+*/
+
+#include <stdio.h>
+//Making a function
+          void swapintegers(int &a,int &b){
+          int temp = a;
+          a = b;
+          b = temp;
+        }
+
+int main () {
+//Printing the values
+          int num1;
+          int num2;
+          printf("Enter the first number: ");
+          scanf("%d", &num1);
+          printf("Enter the second number: ");
+          scanf("%d", &num2);
+
+//Printing the original values 
+          printf("Before swapping: First number= %d\nSecond number= %d\n", num1,num2); 
+      
+//Calling the function
+          swapintegers(&num1,&num2);
+
+//Output
+          printf("Now, the first number= %d\nSecond number= %d\n", num1,num2);
+return 0;
+}//end main
