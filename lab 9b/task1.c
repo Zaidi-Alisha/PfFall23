0beba327388ae3f8e61d58b0eee84d8182003e18
Zaidi-Alisha <142868085+Zaidi-Alisha@users.noreply.github.com>
2023-10-31 09:53:32 +0500

    Add files via upload

---
+++ lab 9b/task1.c
@@ -0,0 +1,45 @@
+/**
+/**
+ *Programmer: Alisha Zaidi
+ *Date: 31 October, 2023
+ *Desc: Question 3
+*/
+
+#include <stdio.h>
+
+int main (){
+  
+    int N;
+    int IntArr[N];
+    int i; //so that i works outside for loop as well
+    char CharArr[N];
+    long long int LongIntArr[N];
+    int *ptr; //reading addresses
+
+//Input for the number of elements in the array 
+    printf("Enter the size of the array: ");
+    scanf("%d", &N);
+  printf("The integer array: \n");
+    for (int i=0; i < N; i++){
+     printf("Enter the number of elements in the array: ", i + 1);
+     scanf("%d", &IntArr[i]);
+     }
+ 
+  printf("The character array: \n");
+ for (int i=0; i < N; i++){
+     printf("Enter the number of elements in the array: ", i + 1);
+     scanf(" %c", &CharArr[i]);
+     }
+     
+ printf("The long long integer array: \n");
+    for (int i=0; i < N; i++){
+     printf("Enter the number of elements in the array: ", i + 1);
+     scanf("%lld", &LongIntArr[i]);
+     }
+
+     int *ptr = IntArr[N];
+       for (int i=0; i < N; i++){
+       printf("%d",*ptr);
+       ptr ++;
+    }
+}//end main
