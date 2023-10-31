6d268997320ce843faec056ad1e94d352256428d
Zaidi-Alisha <142868085+Zaidi-Alisha@users.noreply.github.com>
2023-10-31 10:07:32 +0500

    Update task1.c

--- lab 9b/task1.c
+++ lab 9b/task1.c
@@ -2,44 +2,29 @@
 /**
  *Programmer: Alisha Zaidi
  *Date: 31 October, 2023
- *Desc: Question 3
+ *Desc: Lab 9b Question 1
 */
 
 #include <stdio.h>
-
-int main (){
-  
-    int N;
-    int IntArr[N];
-    int i; //so that i works outside for loop as well
-    char CharArr[N];
-    long long int LongIntArr[N];
-    int *ptr; //reading addresses
-
-//Input for the number of elements in the array 
-    printf("Enter the size of the array: ");
-    scanf("%d", &N);
-  printf("The integer array: \n");
-    for (int i=0; i < N; i++){
-     printf("Enter the number of elements in the array: ", i + 1);
-     scanf("%d", &IntArr[i]);
-     }
- 
-  printf("The character array: \n");
- for (int i=0; i < N; i++){
-     printf("Enter the number of elements in the array: ", i + 1);
-     scanf(" %c", &CharArr[i]);
-     }
-     
- printf("The long long integer array: \n");
-    for (int i=0; i < N; i++){
-     printf("Enter the number of elements in the array: ", i + 1);
-     scanf("%lld", &LongIntArr[i]);
-     }
-
-     int *ptr = IntArr[N];
-       for (int i=0; i < N; i++){
-       printf("%d",*ptr);
-       ptr ++;
+int factorial(int a) {
+    if (a == 0) {
+        return 1;
+    } else {
+        return a * factorial(a - 1);
     }
-}//end main
+}
+
+int main() {
+    int num;
+    printf("Enter the integer: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        int product = factorial(num);
+        printf("Factorial of %d is %d\n", num, product);
+    }
+
+    return 0;
+}
