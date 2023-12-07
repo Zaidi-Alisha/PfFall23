aafa4e29de751437cdaf87e569acdd4a652b7e94
Zaidi-Alisha <142868085+Zaidi-Alisha@users.noreply.github.com>
2023-12-07 23:18:10 +0530

    Update Q2.c

--- Assignment 3/Q2.c
+++ Assignment 3/Q2.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
+//Using structure to record employee data
 typedef struct {
     char name[50];
     char role[50];
@@ -10,11 +11,13 @@ typedef struct {
     int creativity;
 } Employee;
 
+//Using struct to define department
 typedef struct {
     Employee employees[5];
 } Department;
 
-char *generate_name() {
+//Making a function for generating the name
+char *gen_name() {
     static char name[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     char *new_name = malloc(sizeof(char) * 6);
     int length = rand() % 6 + 3;
@@ -25,27 +28,31 @@ char *generate_name() {
     return new_name;
 }
 
+//Making function for assigning department according to name
 void init_employee(Employee *employee) {
-    employee->name = generate_name();
-    sprintf(employee->role, "%s", employee->name[0] == 'a' ? "Director" :
-                            employee->name[0] == 'b' ? "Executive" :
-                            employee->name[0] == 'c' ? "Manager" :
-                            employee->name[0] == 'd' ? "Employee" : "Trainee");
+    employee->name = gen_name();
+    sprintf(employee->role, "%s", employee->name[0] == 'b' ? "Director" :
+                            employee->name[0] == 'c' ? "Executive" :
+                            employee->name[0] == 'd' ? "Manager" :
+                            employee->name[0] == 'a' ? "Employee" : "Trainee");
     employee->communication = rand() % 100 + 1;
     employee->teamwork = rand() % 100 + 1;
     employee->creativity = rand() % 100 + 1;
 }
 
+//Making function for iteration of above function
 void init_department(Department *department) {
     for (int i = 0; i < 5; i++) {
         init_employee(&department->employees[i]);
     }
 }
 
+//Function for getting the sums of all departments
 int sum_employee(Employee employee) {
     return employee.communication + employee.teamwork + employee.creativity;
 }
 
+//Function for comparing the sums
 int sum_department(Department department) {
     int sum = 0;
     for (int i = 0; i < 5; i++) {
@@ -54,6 +61,7 @@ int sum_department(Department department) {
     return sum;
 }
 
+//Making function to find the best department
 int find_best_department(Department departments[], int n) {
     int max_sum = 0;
     int best_department = -1;
@@ -67,6 +75,7 @@ int find_best_department(Department departments[], int n) {
     return best_department;
 }
 
+//Function for printing employee data
 void print_employee(Employee employee) {
     printf("%-20s %-20s %-10d %-10d %-10d\n", employee.name, employee.role, employee.communication, employee.teamwork, employee.creativity);
 }
@@ -100,63 +109,5 @@ int main() {
     return 0;
 }
     res[pos] = max;
-}
+}//end main
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <hello.txt>\n", argv[0]);
-        return 1;
-    }
-
-    FILE *file = fopen(argv[1], "r");
-    if (file == NULL) {
-        printf("Could not open file %s\n", argv[1]);
-        return 1;
-    }
-
-    int n;
-    fscanf(file, "%d", &n);
-    n = (int)pow(2, n);
-
-    int arr[n * n];
-    for (int i = 0; i < n * n; i++) {
-        fscanf(file, "%d", &arr[i]);
-    }
-
-    int res[n * n / 4];
-    int i;
-    for (int i = 0; i < n; i += 2) {
-        for (int j = 0; j < n; j += 2) {
-            int sub_matrix[4];
-            sub_matrix[0] = arr[i * n + j];
-            sub_matrix[1] = arr[i * n + j + 1];
-            sub_matrix[2] = arr[(i + 1) * n + j];
-            sub_matrix[3] = arr[(i + 1) * n + j + 1];
-            find_max(2, sub_matrix, res, i / 2 * n / 2 + j / 2);
-        }
-    }
-
-    for (int i = 0; i < n * n / 4; i++) {
-        printf("%d ", res[i]);
-    }
-    printf("\n");
-
-    fclose(file);
-    return 0;
-
-FILE *fp;
-char ch;
-fp = fopen("hello.txt", "w");
-printf("Enter data");
-while((ch = getchar())!= EOF) {
-  putc(ch,fp);
-}
-fclose(fp);
-fp = fopen("hello.txt", "r");
-
-while( (ch = getc (fp))!= EOF) {
-  printf("%c",ch);
-  
-fclose(fp);
-}
-}//end main 
